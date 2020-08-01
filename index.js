f36349df87e2ec66b56d91b93bb47a2a363c2329
Outvi V <git@outv.im>
2020-08-01 12:27:49 +0800

    feat: Support YAML

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const fs = require("fs");
+const yaml = require("js-yaml");
 
 function findAndReturnJSON(text) {
   return JSON.parse(
@@ -20,6 +21,7 @@ function main() {
   try {
     const comment = core.getInput("comment");
     const target = core.getInput("target");
+    const format = core.getInput("format");
     let to_insert = {};
     try {
       to_insert = findAndReturnJSON(comment);
@@ -30,12 +32,20 @@ function main() {
     let old_content = [];
     try {
       const content = fs.readFileSync(target, "utf-8");
-      old_content = JSON.parse(content);
+      if (format.toLowerCase() == "yaml") {
+        old_content = yaml.safeLoad(content);
+      } else {
+        old_content = JSON.parse(content);
+      }
     } catch (e) {
       core.setFailed(`Can't find or parse target JSON at ${target}: ${e}`);
     }
     old_content.push(to_insert);
-    fs.writeFileSync(target, JSON.stringify(old_content, null, 4));
+    if (format.toLowerCase() === "yaml") {
+      fs.writeFileSync(target, yaml.safeDump(old_content));
+    } else {
+      fs.writeFileSync(target, JSON.stringify(old_content, null, 4));
+    }
     core.setOutput("username", findUserName(comment));
   } catch (error) {
     core.setFailed(error.message);
